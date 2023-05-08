279660eae0b50a5b0fdc798af5b1b910e953d577
Rafał Płoszański <rafalploszanski@interia.pl>
2023-05-08 22:34:48 +0200

    create api for url shortener
    file changes while keeping pep8

--- api/shortener/serializers.py
+++ api/shortener/serializers.py
@@ -1,15 +1,27 @@
 from rest_framework import serializers
 
 from .models import URL
+import shortuuid
 
 
 class URLSerializer(serializers.ModelSerializer):
+    short_url = serializers.SerializerMethodField()
+
     class Meta:
         model = URL
-        fields = ('id', 'short_url')
+        fields = ("short_url",)
+
+    def get_short_url(self, obj):
+        return self.context.get("request").build_absolute_uri(obj.short_url)
 
 
 class OriginalURLSerializer(serializers.ModelSerializer):
     class Meta:
         model = URL
-        fields = ('original_url', )
+        fields = ("url",)
+
+    def save(self, **kwargs):
+        original_url = self.validated_data.get("url")
+        url = URL(url=original_url, short_url=shortuuid.uuid(name=original_url))
+        url.save()
+        return url
--- api/shortener/views.py
+++ api/shortener/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from . import models, serializers
+from .serializers import URLSerializer
+
+
+class CreateShortURLAPIView(generics.CreateAPIView):
+    queryset = models.URL.objects.all()
+    serializer_class = serializers.OriginalURLSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+
+        short_url = URLSerializer(instance=instance, context={"request": request})
+
+        return Response(data=short_url.data, status=status.HTTP_201_CREATED)
