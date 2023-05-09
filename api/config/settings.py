b82f3ed3d622c9e5762502f1120b77ce935c1703
Rafał Płoszański <rafalploszanski@interia.pl>
2023-05-09 22:26:10 +0200

    revert to sqlite3 for heroku without payment

--- api/config/settings.py
+++ api/config/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
-
-DATABASES = {"default": dj_database_url.config(default=DB_CONNECTION_STRING)}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
