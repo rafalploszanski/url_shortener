4ac719c783566d9faf0c09007bb8f3e9eeaca687
Rafał Płoszański <rafalploszanski@interia.pl>
2023-05-10 18:56:06 +0200

    configure swagger-ui

--- api/config/settings.py
+++ api/config/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-
+from config.swagger_settings import SPECTACULAR_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = "secret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -114,7 +109,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -138,17 +132,10 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
-SPECTACULAR_SETTINGS = {
-    "TITLE": "URL Shortener API",
-    "DESCRIPTION": "Best URL Shortener API in the world",
-    "VERSION": "1.0.0",
-    "SERVE_INCLUDE_SCHEMA": False,
-}
-
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 ALLOWED_HOSTS = [
     "localhost",
-    "http://127.0.0.1",
+    "127.0.0.1",
     "protected-brushlands-01714.herokuapp.com",
     "https://protected-brushlands-01714.herokuapp.com/",
 ]
--- api/shortener/views.py
+++ api/shortener/views.py
@@ -10,6 +10,24 @@ from .serializers import URLSerializer
 
 
 class CreateShortURLApiView(generics.CreateAPIView):
+    """
+    create:
+    Creates a short URL from the provided original URL.
+
+    Expects JSON data in the format:
+    {
+        "url": "http://protected-brushlands-01714.herokuapp.com/abcd/"
+    }
+
+    Where "url" is the original URL that is to be shortened.
+
+    The response contains the original URL and the shortened URL:
+    {
+        "url": "http://example.com",
+        "short_url": "http://your-domain.com/abcd"
+    }
+    """
+
     queryset = models.URL.objects.all()
     serializer_class = serializers.OriginalURLSerializer
 
@@ -24,6 +42,18 @@ class CreateShortURLApiView(generics.CreateAPIView):
 
 
 class ShortUrlRedirectView(RedirectView):
+    """
+    get:
+    Redirects the user to the original URL from the provided short URL.
+
+    Expects the short URL to be part of the path, like:
+    http://protected-brushlands-01714.herokuapp.com/abcd/
+
+    Where "abcd" is the short URL.
+
+    The response is a HTTP 302 redirect to the original URL.
+    """
+
     def get_redirect_url(self, *args: Any, short_url: str, **kwargs: Any) -> str | None:
         url = get_object_or_404(models.URL, short_url=short_url)
 
